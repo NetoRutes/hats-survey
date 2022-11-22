113d7bedb45210a8c419faf8d31d43bbe79a245b
Wellington Dyon Fuck Rutes <wellington.rutes@rga.com>
2022-11-21 22:18:36 -0300

    initial commit.

---
+++ hats-survey.js
@@ -0,0 +1,14 @@
+/*
+ * HaTS Survey Code
+ *
+ * http://go/hats/client/concepts
+ *
+ */
+
+class HaTSSurvey {
+    constructor() {
+      window.console.warn('OPA! HaTS API is loaded ;).');
+    }
+  }
+  
+  export {HaTSSurvey};
